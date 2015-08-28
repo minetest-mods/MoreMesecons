a673ceb374e65cf46ecec04b2a1ea32fb3120828
palige <plangrognet@laposte.net>
2015-08-28 12:24:33 +0200

    Fix a bug for temporarygate and add Signalchanger

---
+++ moremesecons_signalchanger/init.lua
@@ -0,0 +1,71 @@
+local nodebox = {
+	type = "fixed",
+	fixed = {{-8/16, -8/16, -8/16, 8/16, -7/16, 8/16 }},
+}
+
+local function signalchanger_get_output_rules(node)
+	local rules = {{x=1, y=0, z=0}}
+	for i = 0, node.param2 do
+		rules = mesecon.rotate_rules_left(rules)
+	end
+	return rules
+end
+
+local function signalchanger_get_input_rules(node)
+	local rules = {{x=0, y=0, z=1, name="input_on"}, {x=0, y=0, z=-1, name="input_off"}}
+	for i = 0, node.param2 do
+		rules = mesecon.rotate_rules_left(rules)
+	end
+	return rules
+end
+
+local update = function(pos, node, link, newstate)
+	local meta = minetest.get_meta(pos)
+	meta:set_int(link.name, newstate == "on" and 1 or 0)
+	local input_on = meta:get_int("input_on") == 1
+	local input_off = meta:get_int("input_off") == 1
+	if input_on then
+		mesecon.receptor_on(pos, signalchanger_get_output_rules(node))
+		minetest.swap_node(pos, {name = "moremesecons_signalchanger:signalchanger_on", param2 = node.param2})
+	elseif input_off then
+		mesecon.receptor_off(pos, signalchanger_get_output_rules(node))
+		minetest.swap_node(pos, {name = "moremesecons_signalchanger:signalchanger_off", param2 = node.param2})
+	end
+end
+
+mesecon.register_node("moremesecons_signalchanger:signalchanger", {
+	description = "Signal Changer",
+	inventory_image = "moremesecons_signalchanger_off.png",
+	groups = {dig_immediate = 2},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drawtype = "nodebox",
+	selection_box = nodebox,
+	node_box = nodebox,
+},{
+	groups = {dig_immediate = 2},
+	mesecons = {
+		receptor = {
+			state = mesecon.state.off,
+			rules = signalchanger_get_output_rules
+		},
+		effector = {
+			rules = signalchanger_get_input_rules,
+			action_change = update
+		},
+	},
+	tiles = {"moremesecons_signalchanger_off.png"},
+},{
+	groups = {dig_immediate = 2, not_in_creative_inventory = 1},
+	mesecons = {
+		receptor = {
+			state = mesecon.state.on,
+			rules = signalchanger_get_output_rules,
+		},
+		effector = {
+			rules = signalchanger_get_input_rules,
+			action_change = update,
+		},
+	},
+	tiles = {"moremesecons_signalchanger_on.png"},
+})
---
+++ moremesecons_temporarygate/init.lua
@@ -0,0 +1,115 @@
+-- MOREMESECONS TEMPORARY GATE
+
+local temporarygate_get_output_rules = function(node)
+	local rules = {{x = 0, y = 0, z = 1}}
+	for i = 0, node.param2 do
+		rules = mesecon.rotate_rules_left(rules)
+	end
+	return rules
+end
+
+local temporarygate_get_input_rules = function(node)
+	local rules = {{x = 0, y = 0, z = -1}}
+	for i = 0, node.param2 do
+		rules = mesecon.rotate_rules_left(rules)
+	end
+	return rules
+end
+
+-- Functions that are called after the delay time
+
+local temporarygate_activate = function(pos, node)
+	local def = minetest.registered_nodes[node.name]
+	local meta = minetest.get_meta(pos)
+	local time = tonumber(meta:get_string("time"))
+	if time ~= nil then
+		minetest.swap_node(pos, {name = "moremesecons_temporarygate:temporarygate_on", param2 = node.param2})
+		mesecon.receptor_on(pos)
+		minetest.after(time, function(pos, node)
+			mesecon.receptor_off(pos)
+			minetest.swap_node(pos, {name = "moremesecons_temporarygate:temporarygate_off", param2 = node.param2})
+		end, pos, node)
+	end
+end
+
+boxes = {{ -6/16, -8/16, -6/16, 6/16, -7/16, 6/16 },		-- the main slab
+
+	 { -2/16, -7/16, -4/16, 2/16, -26/64, -3/16 },		-- the jeweled "on" indicator
+	 { -3/16, -7/16, -3/16, 3/16, -26/64, -2/16 },
+	 { -4/16, -7/16, -2/16, 4/16, -26/64, 2/16 },
+	 { -3/16, -7/16,  2/16, 3/16, -26/64, 3/16 },
+	 { -2/16, -7/16,  3/16, 2/16, -26/64, 4/16 },
+
+	 { -6/16, -7/16, -6/16, -4/16, -27/64, -4/16 },		-- the timer indicator
+	 { -8/16, -8/16, -1/16, -6/16, -7/16, 1/16 },		-- the two wire stubs
+	 { 6/16, -8/16, -1/16, 8/16, -7/16, 1/16 }}
+
+mesecon.register_node("moremesecons_temporarygate:temporarygate", {
+	description = "Temporary Gate",
+	drawtype = "nodebox",
+	inventory_image = "moremesecons_temporarygate_off.png",
+	wield_image = "moremesecons_temporarygate_off.png",
+	walkable = true,
+	selection_box = {
+		type = "fixed",
+		fixed = { -8/16, -8/16, -8/16, 8/16, -6/16, 8/16 },
+	},
+	node_box = {
+		type = "fixed",
+		fixed = boxes
+	},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = true,
+	is_ground_content = true,
+	sounds = default.node_sound_stone_defaults(),
+	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+   		meta:set_string("formspec", "field[time;time;${time}]")
+	end,
+	on_receive_fields = function(pos, formname, fields, sender)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("time", fields.time)
+	end
+},{
+		tiles = {
+			"moremesecons_temporarygate_off.png",
+			"moremesecons_temporarygate_bottom.png",
+			"moremesecons_temporarygate_ends_off.png",
+			"moremesecons_temporarygate_ends_off.png",
+			"moremesecons_temporarygate_sides_off.png",
+			"moremesecons_temporarygate_sides_off.png"
+		},
+		groups = {bendy=2,snappy=1,dig_immediate=2},
+		mesecons = {
+			receptor =
+			{
+				state = mesecon.state.off,
+				rules = temporarygate_get_output_rules
+			},
+			effector =
+			{
+				rules = temporarygate_get_input_rules,
+				action_on = temporarygate_activate
+			}
+		},
+},{
+		tiles = {
+			"moremesecons_temporarygate_on.png",
+			"moremesecons_temporarygate_bottom.png",
+			"moremesecons_temporarygate_ends_on.png",
+			"moremesecons_temporarygate_ends_on.png",
+			"moremesecons_temporarygate_sides_on.png",
+			"moremesecons_temporarygate_sides_on.png"
+		},
+		groups = {bendy=2,snappy=1,dig_immediate=2, not_in_creative_inventory=1},
+		mesecons = {receptor = {state = mesecon.state.on, rules = temporarygate_get_output_rules}},
+})
+
+minetest.register_craft({
+	output = "moremesecons_temporarygate:temporarygate_off",
+	recipe = {
+		{"mesecons_torch:mesecon_torch_on", "group:mesecon_conductor_craftable", "mesecons_torch:mesecon_torch_on"},
+		{"default:wood","default:wood", "default:wood"},
+	}
+})
