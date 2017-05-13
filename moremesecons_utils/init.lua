d542d15fa47b2c0fb9ab0e09b293288ca05426a1
AntumDeluge <antumdeluge@gmail.com>
2017-05-13 01:52:42 -0700

    Replace deprecated methods:
    - 'setting_get' with 'settings:get'
    - 'setting_getbool' with 'settings:get_bool'

--- moremesecons_utils/init.lua
+++ moremesecons_utils/init.lua
@@ -4,15 +4,15 @@ function moremesecons.setting(modname, settingname, default, min)
 	local setting = "moremesecons_" .. modname .. "." .. settingname
 
 	if type(default) == "boolean" then
-		local ret = minetest.setting_getbool(setting)
+		local ret = minetest.settings:get_bool(setting)
 		if ret == nil then
 			ret = default
 		end
 		return ret
 	elseif type(default) == "string" then
-		return minetest.setting_get(setting) or default
+		return minetest.settings:get(setting) or default
 	elseif type(default) == "number" then
-		local ret = tonumber(minetest.setting_get(setting)) or default
+		local ret = tonumber(minetest.settings:get(setting)) or default
 		if ret ~= ret then -- NaN
 			minetest.log("warning", "[moremesecons_"..modname.."]: setting '"..setting.."' is NaN. Set to default value ("..tostring(default)..").")
 			ret = default
